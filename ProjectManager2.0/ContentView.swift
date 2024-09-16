2c205ed403c21915a05628e11dab3b8cb3fbfcb6
Chris Gonzalez <christogonz92@gmail.com>
2024-09-16 17:20:22 +0200

    Initial Commit

---
+++ ProjectManager2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectManager2.0
+//
+//  Created by Christopher Gonzalez on 2024-09-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectManager2.0/ProjectManager2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManager2_0App.swift
+//  ProjectManager2.0
+//
+//  Created by Christopher Gonzalez on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManager2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
